badbecb404c9f4255c488e358fec61643b4b183f
Patryk Gamrat <Patryk.gam@gmail.com>
2025-02-01 03:07:46 +0100

    Rotate buttons based on orientation

--- app/src/main/java/com/example/cameratopdf/MainActivity.kt
+++ app/src/main/java/com/example/cameratopdf/MainActivity.kt
@@ -6,16 +6,16 @@ import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import com.example.cameratopdf.databinding.ActivityMainBinding
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
 import android.util.Log
 import android.util.TypedValue
+import android.view.OrientationEventListener
 import android.view.Surface
 import android.view.WindowManager
 import android.widget.Toast
@@ -28,10 +28,12 @@ import androidx.constraintlayout.widget.ConstraintLayout
 class MainActivity : AppCompatActivity() {
 
     enum class Rotation {
-        PORTRAIT, LANDSCAPE, REVERSE_LANDSCAPE, UNKNOWN
+        PORTRAIT, LANDSCAPE, REVERSE_PORTRAIT, REVERSE_LANDSCAPE
     }
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var orientationEventListener: OrientationEventListener
+    private var selectedCamera = CameraSelector.DEFAULT_BACK_CAMERA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Inflate view and adjust it
@@ -39,30 +41,47 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemInsets = insets.getInsets(
-                WindowInsetsCompat.Type.systemBars()
-            )
-            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = systemInsets.left
-                bottomMargin = systemInsets.bottom
-                rightMargin = systemInsets.right
+
+        // Device rotation listener
+        orientationEventListener = object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation == ORIENTATION_UNKNOWN) return
+
+                // Determine the rotation of the screen
+                val rotation = when (orientation) {
+                    in 45..134 -> Rotation.REVERSE_LANDSCAPE
+                    in 135..224 -> Rotation.REVERSE_PORTRAIT
+                    in 225..314 -> Rotation.LANDSCAPE
+                    else -> Rotation.PORTRAIT
+                }
+                rotateView(rotation)
             }
-            WindowInsetsCompat.CONSUMED
         }
-        adjustViewToRotation()
+        if (orientationEventListener.canDetectOrientation()) {
+            orientationEventListener.enable()
+        } else {
+            Log.e(TAG, "Orientation detection not supported")
+        }
 
         // Request camera permissions and start camera
         if (allPermissionsGranted()) {
-            startCamera()
-        }
-        else {
+            startCamera(selectedCamera)
+        } else {
             requestPermissions()
+        }
+
+        // Switch camera on button press
+        binding.switchCameraButton.setOnClickListener {
+            selectedCamera = if (selectedCamera == CameraSelector.DEFAULT_FRONT_CAMERA)
+                CameraSelector.DEFAULT_BACK_CAMERA
+            else
+                CameraSelector.DEFAULT_FRONT_CAMERA
+            startCamera(selectedCamera)
         }
     }
 
     // Starts camera preview
-    private fun startCamera() {
+    private fun startCamera(selector: CameraSelector) {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
@@ -75,9 +94,8 @@ class MainActivity : AppCompatActivity() {
                 .also {
                     it.surfaceProvider = binding.viewFinder.surfaceProvider
                 }
-
             // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            val cameraSelector = selector
 
             try {
                 // Unbind use cases before rebinding
@@ -85,9 +103,10 @@ class MainActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview
+                )
 
-            } catch(exc: Exception) {
+            } catch (exc: Exception) {
                 Log.e(TAG, "Camera preview failed", exc)
             }
 
@@ -98,7 +117,8 @@ class MainActivity : AppCompatActivity() {
     private fun requestPermissions() = activityResultLauncher.launch(REQUIRED_PERMISSIONS)
     private val activityResultLauncher =
         registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions())
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
         { permissions ->
             // Handle Permission granted/rejected
             var permissionGranted = true
@@ -107,11 +127,13 @@ class MainActivity : AppCompatActivity() {
                     permissionGranted = false
             }
             if (!permissionGranted) {
-                Toast.makeText(baseContext,
+                Toast.makeText(
+                    baseContext,
                     resources.getString(R.string.camera_permission_denied),
-                    Toast.LENGTH_SHORT).show()
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
-                startCamera()
+                startCamera(selectedCamera)
             }
         }
 
@@ -126,132 +148,20 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "CameraToPdf"
         private val REQUIRED_PERMISSIONS =
-            mutableListOf (
+            mutableListOf(
                 Manifest.permission.CAMERA,
             ).toTypedArray()
     }
 
-    // Get device rotation
-    private fun getRotation(context: Context): Rotation {
-        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val rotation: Int
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            rotation = this.display.rotation
+    // Rotate view based on device orientation
+    private fun rotateView(rotation: Rotation) {
+        val rotationDegree = when (rotation) {
+            Rotation.PORTRAIT -> 0
+            Rotation.LANDSCAPE -> 90
+            Rotation.REVERSE_PORTRAIT -> 180
+            Rotation.REVERSE_LANDSCAPE -> 270
         }
-        else {
-            @Suppress("DEPRECATION")
-            rotation = windowManager.defaultDisplay.rotation
-        }
-        return when (rotation) {
-            Surface.ROTATION_0 -> Rotation.PORTRAIT
-            Surface.ROTATION_90 -> Rotation.LANDSCAPE
-            Surface.ROTATION_270 -> Rotation.REVERSE_LANDSCAPE
-            else -> Rotation.UNKNOWN
-        }
-    }
-
-    // Resets constraint layout parameters, used when adjusting view to rotation
-    private fun resetLayoutParams() {
-        binding.imageCaptureButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-            startToStart = ConstraintLayout.LayoutParams.UNSET
-            startToEnd = ConstraintLayout.LayoutParams.UNSET
-            endToStart = ConstraintLayout.LayoutParams.UNSET
-            endToEnd = ConstraintLayout.LayoutParams.UNSET
-            topToTop = ConstraintLayout.LayoutParams.UNSET
-            topToBottom = ConstraintLayout.LayoutParams.UNSET
-            bottomToTop = ConstraintLayout.LayoutParams.UNSET
-            bottomToBottom = ConstraintLayout.LayoutParams.UNSET
-            topMargin = 0
-            rightMargin = 0
-            bottomMargin = 0
-            leftMargin = 0
-            horizontalBias = 0.5f
-            verticalBias = 0.5f
-        }
-        binding.switchCameraButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-            startToStart = ConstraintLayout.LayoutParams.UNSET
-            startToEnd = ConstraintLayout.LayoutParams.UNSET
-            endToStart = ConstraintLayout.LayoutParams.UNSET
-            endToEnd = ConstraintLayout.LayoutParams.UNSET
-            topToTop = ConstraintLayout.LayoutParams.UNSET
-            topToBottom = ConstraintLayout.LayoutParams.UNSET
-            bottomToTop = ConstraintLayout.LayoutParams.UNSET
-            bottomToBottom = ConstraintLayout.LayoutParams.UNSET
-            topMargin = 0
-            rightMargin = 0
-            bottomMargin = 0
-            leftMargin = 0
-            horizontalBias = 0.5f
-            verticalBias = 0.5f
-        }
-    }
-
-    // Adjusts view to current device rotation
-    private fun adjustViewToRotation() {
-        resetLayoutParams()
-        val rotation = getRotation(this)
-        when (rotation) {
-            Rotation.PORTRAIT -> {
-                Log.i("TEST", "Rotation: portrait")
-                binding.imageCaptureButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                    bottomToBottom = binding.container.id
-                    endToEnd = binding.container.id
-                    startToStart = binding.container.id
-                    bottomMargin = dpToPx(48f)
-                }
-                binding.switchCameraButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                    bottomToBottom = binding.container.id
-                    endToEnd = binding.container.id
-                    startToEnd = binding.imageCaptureButton.id
-                    bottomMargin = dpToPx(32f)
-                    horizontalBias = 0.7f
-                }
-            }
-            Rotation.LANDSCAPE -> {
-                Log.i("TEST", "Rotation: landscape")
-                binding.imageCaptureButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                    endToEnd = binding.container.id
-                    topToTop = binding.container.id
-                    bottomToBottom = binding.container.id
-                    rightMargin = dpToPx(48f)
-                }
-                binding.switchCameraButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                    endToEnd = binding.container.id
-                    topToTop = binding.container.id
-                    bottomToTop = binding.imageCaptureButton.id
-                    rightMargin = dpToPx(32f)
-                    verticalBias = 0.3f
-                }
-            }
-            Rotation.REVERSE_LANDSCAPE -> {
-                Log.i("TEST", "Rotation: reverse landscape")
-                binding.imageCaptureButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                    startToStart = binding.container.id
-                    topToTop = binding.container.id
-                    bottomToBottom = binding.container.id
-                    leftMargin = dpToPx(48f)
-                }
-                binding.switchCameraButton.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                    startToStart = binding.container.id
-                    topToBottom = binding.imageCaptureButton.id
-                    bottomToBottom = binding.container.id
-                    leftMargin = dpToPx(32f)
-                    verticalBias = 0.7f
-                }
-            }
-            else -> Unit
-        }
-    }
-
-    // Converts dp to px
-    private fun Context.dpToPx(dp: Float): Int {
-        return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics
-        ).toInt()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        adjustViewToRotation()
+        binding.switchCameraButton.rotation = rotationDegree.toFloat()
+        binding.settingsButton.rotation = rotationDegree.toFloat()
     }
 }
